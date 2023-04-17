82badb8fbc6335390566c6900fd073ab443b9dc6
giovani <giovaniocan@gmail.com>
2023-04-17 15:44:10 -0300

    responsive OK

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,9 @@
+import { NavLink } from "./NavLink";
 import { Logo } from "./icons/Logo";
 
 export function Sidebar() {
     return (
-        <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 p-6" >
+        <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 p-6 hidden lg:block" >
             <Logo className="text-white h-4 w-auto-" />
 
             <nav className=" mt-10 flex flex-col gap-6 ">
@@ -12,13 +13,13 @@ export function Sidebar() {
                     </strong>
 
                     <div className=" text-sm flex flex-col pl-2 ">
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href=""> Introduction</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href=""> Quick Start</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">SDKs</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">Authentication</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">PAgination</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">Errors</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">WekRooks</a>
+                        <NavLink href="/test/a"> Introduction</NavLink>
+                        <NavLink href="/test/b"> Quick Start</NavLink>
+                        <NavLink href="/test/c">SDKs</NavLink>
+                        <NavLink href="/test/d">Authentication</NavLink>
+                        <NavLink href="/test/e">PAgination</NavLink>
+                        <NavLink href="/test/f">Errors</NavLink>
+                        <NavLink href="/test/g">WekRooks</NavLink>
                     </div>
                 </div>
 
@@ -28,11 +29,11 @@ export function Sidebar() {
                     </strong>
 
                     <div className=" text-sm flex flex-col pl-2 ">
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">Contacts</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">Conversation</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">Massages</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">Groups</a>
-                        <a className=" px-4 py-1.5 border-l border-white/5 text-zinc-400 hover:text-white transition-colors " href="">Attachments</a>
+                        <NavLink href="/test/h">Contacts</NavLink>
+                        <NavLink href="/test/i">Conversation</NavLink>
+                        <NavLink href="/test/j">Massages</NavLink>
+                        <NavLink href="/test/k">Groups</NavLink>
+                        <NavLink href="/test/l">Attachments</NavLink>
 
                     </div>
                 </div>
@@ -40,3 +41,5 @@ export function Sidebar() {
         </aside>
     )
 }
+
+{/* 'hidden lg:block' aqui estamos falando que ela por natureza é hidden, ou seja, não aparece, mas quando a largura for large ele fico bloqueado na tela */}
